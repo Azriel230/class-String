1ff61ced9856c218514fd878feeedc988fdd1f73
Alexey <91151767+Azriel230@users.noreply.github.com>
2022-11-23 17:14:59 +0300

    Update string.h

--- string.h
+++ string.h
@@ -11,17 +11,36 @@ public:
 	String(const String& str_);
 	~String();
 
-	String& operator=(const String& str_)
-	{
-		if (this == &str_)
-			return *this;
-		Clear();
-		Copy(str_);
-		return *this;
-	}
+	String& operator=(const String& str_);
+
+	bool operator==(const String& str_);
+	bool operator!=(const String& str_);
+	bool operator<(const String & str_);
+	bool operator>(const String& str_);
+	bool operator<=(const String& str_);
+	bool operator>=(const String& str_);
 
 	String operator+(const String& str_);
 	String& operator+=(const String& str_);
+
+	//Capacity:
+	bool IsEmpty();
+	int Length();
+
+	//Access
+	char& At(int index_);
+	char& Front();
+	char& Back();
+	char& operator[](int index_);
+
+	//Modifiers
+	//void Clear();
+	String& Insert(char sym_, int pos_);
+	String& Erase(int firstPos_, int lastPos_);
+	String& Push_back(char sym_);
+	void Resize(int size_);
+	void Swap(String str_);
+	String& Append(const String& str_);
 
 	friend std::ostream& operator<<(std::ostream& stream, const String& str_);
 	friend std::istream& operator>>(std::istream& stream, String& str_);
