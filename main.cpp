3a4232b8aee2b004818a0c186551a22cf74eb346
Alexey <91151767+Azriel230@users.noreply.github.com>
2022-12-04 17:48:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include "string.h"
+#include "SException.h"
 
 int main()
 {
-	String str1;
-	std::cin >> str1;
-	std::cout << std::endl << "str1 = " << str1 << std::endl;
+	try
+	{
+		String str1(5, "Hello");
+		//std::cin >> str1;
+		std::cout << std::endl << "str1 = " << str1 << std::endl;
 
-	String str2;
-	std::cin >> str2;
-	std::cout << std::endl << "str2 = " << str2 << std::endl;
+		String str2(6, "World!");
+		//std::cin >> str2;
+		std::cout << std::endl << "str2 = " << str2 << std::endl;
 
-	String str;
-	str = str1 + str2;
-	std::cout << std::endl << "str = " << str << std::endl;
+		String str;
+		str = str1 + str2;
+		std::cout << std::endl << "str = " << str << std::endl;
+
+		std::cout << std::endl << str[10];
+		std::cout << std::endl << str[100];
+	}
+	catch (SException obj)
+	{
+		std::cout << obj;
+	}
 
 	return 0;
 }
